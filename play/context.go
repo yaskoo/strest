40ef2112eed8baaf7596989b8699c3f80652e5e5
Yasko <io.yasko@gmail.com>
2017-05-02 20:29:52 +0300

    fix docs for context

--- play/context.go
+++ play/context.go
@@ -10,6 +10,6 @@ type Context struct {
 	// Res holds the reponse data for the current request
 	Res *Response
 
-	// Register is string/string map holding custom data
+	// Register holds custom data registered from individual steps
 	Register map[string]string
 }
